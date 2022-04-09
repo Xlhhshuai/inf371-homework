2e4b7714c379f0d11b3ea3bd98501fd13b1e3a70
Xlhhshuai <1132062828@qq.com>
2022-04-09 11:50:26 +0800

    窗口

--- infhomework/1.py
+++ infhomework/1.py
@@ -1,1 +1,13 @@
-print("hello world")
+import pygame
+import sys
+
+pygame.init()                              # 初始化
+
+screen = pygame.display.set_mode((500, 500))  # 窗口尺寸
+
+while True:      # 绘制画面死循环
+    pygame.display.update()
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            pygame.quir()
+            sys.exit()         
