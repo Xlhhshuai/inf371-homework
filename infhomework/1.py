f812429c7475a59f1a7d330ca1555b4dacc8e88c
Xlhhshuai <1132062828@qq.com>
2022-04-15 09:39:32 +0800

    小改动

--- infhomework/1.py
+++ infhomework/1.py
@@ -3,11 +3,18 @@ import sys
 
 pygame.init()                              # 初始化
 
-screen = pygame.display.set_mode((500, 500))  # 窗口尺寸
+window = pygame.display.set_mode((500, 500))  # 窗口尺寸
+
+# 设置游戏标题
+pygame.display.set_caption('小游戏')
+
+
 
 while True:      # 绘制画面死循环
     pygame.display.update()
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             pygame.quir()
-            sys.exit()         
+            sys.exit()        
+
+            # 图像显示与运动         
